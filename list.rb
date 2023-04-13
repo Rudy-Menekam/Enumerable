bd71f9d292fe6a385b5a03206131e9ba3a487972
alicemirigo92 <alicemirigo92@gmai.com>
2023-04-13 22:27:43 +0300

    Fix errors

--- list.rb
+++ list.rb
@@ -1,4 +1,4 @@
-require_relative 'my_enumerable'
+require_relative './my_enumerable'
 
 class MyList
   include MyEnumerable
@@ -15,12 +15,12 @@ end
 list = MyList.new(1, 2, 3, 4)
 
 # Test #all?
-list.all? {|e| e < 5}
-list.all? {|e| e > 5}
+list.all? { |e| e < 5 }
+list.all? { |e| e > 5 }
 
 # Test #any?
-list.any? {|e| e == 2}
-list.any? {|e| e == 5}
+list.any? { |e| e == 2 }
+list.any? { |e| e == 5 }
 
 # Test #filter
-list.filter {|e| e.even?}
+list.filter(&:even?)
--- my_enumerable.rb
+++ my_enumerable.rb
@@ -20,3 +20,4 @@ module MyEnumerable
     end
     array
   end
+end
