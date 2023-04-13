e33a1932a317002f09cb52a2ed880c00a9a271e6
alicemirigo92 <alicemirigo92@gmai.com>
2023-04-13 18:06:00 +0300

    Add my enumerable module

---
+++ my_enumerable.rb
@@ -0,0 +1,22 @@
+module MyEnumerable
+  def all?
+    each do |element|
+      return false unless yield element
+    end
+    true
+  end
+
+  def any?
+    each do |element|
+      return true if yield element
+    end
+    false
+  end
+
+  def filter
+    array = []
+    each do |element|
+      array.push(element) if yield element
+    end
+    array
+  end
